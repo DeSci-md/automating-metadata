5b6a3073aeb67bbcf002423902ab6ec9950db6fe
Liam Arbuckle <liam@skinetics.tech>
2023-06-05 16:46:15 +1000

    🐚🧉 ↝ Fixed pressure point in file upload method, add logs from .venv

--- metadataextraction/app.py
+++ metadataextraction/app.py
@@ -1,33 +1,34 @@
 from flask import Flask, request, redirect, url_for
 import os
-
+ 
 app = Flask(__name__)
-
+ 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        if 'file' not in request.files: # Check if the post request has the file
+        # check if the post request has the file part
+        if 'file' not in request.files:
             return redirect(request.url)
         file = request.files['file']
-        if file.filename == '': # Check; if user doesn't upload a file in the request
+        # if user does not select file, browser may submit an empty part without filename
+        if file.filename == '':
             return redirect(request.url)
         if file:
             filename = file.filename
-            file.save(os.path.join(app.root_path + '/uploads', filename))
-            return 'File uploaded successfully'
-        
-        return '''
-            <!doctype html>
-            <html>
-                <body>
-                    <h1>Upload a file</h1>
-                    <form method="POST" enctype="multipart/form-data">
-                        <input type="file" name="file">
-                        <input type="submit" values="Upload">
-                    </form>
-                </body>
-            </html>
-        '''
-
+            file.save(os.path.join(app.root_path, filename))
+            return 'File uploaded successfully.'
+    return '''
+    <!doctype html>
+    <html>
+    <body>
+    <h1>Upload a File</h1>
+    <form method="POST" enctype="multipart/form-data">
+      <input type="file" name="file">
+      <input type="submit" value="Upload">
+    </form>
+    </body>
+    </html>
+    '''
+ 
 if __name__ == '__main__':
     app.run()
