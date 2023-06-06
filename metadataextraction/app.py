20450015d790346992a35405550eb6ab7cf39267
Liam Arbuckle <liam@skinetics.tech>
2023-06-06 16:54:03 +1000

    🥃🧖🏻 ↝ Extract components of uploaded files in node/drive, access them via API

--- metadataextraction/app.py
+++ metadataextraction/app.py
@@ -1,7 +1,18 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
 import os
+import textract
  
 app = Flask(__name__)
+UPLOAD_FOLDER = os.path.join(app.root_path, 'uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ 
+def extract_text_from_file(file_path):
+    try:
+        text = textract.process(file_path).decode('utf-8')
+        return text
+    except Exception as e:
+        print(f"Error extracting text: {e}")
+        return None
  
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -15,8 +26,10 @@ def upload_file():
             return redirect(request.url)
         if file:
             filename = file.filename
-            file.save(os.path.join(app.root_path, filename))
-            return 'File uploaded successfully.'
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            extracted_text = extract_text_from_file(file_path)
+            return render_template('result.html', filename=filename, extracted_text=extracted_text)
     return '''
     <!doctype html>
     <html>
@@ -29,6 +42,15 @@ def upload_file():
     </body>
     </html>
     '''
+ 
+@app.route('/api/text', methods=['GET'])
+def get_extracted_text():
+    filename = request.args.get('filename')
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    extracted_text = extract_text_from_file(file_path)
+    if extracted_text:
+        return extracted_text
+    return 'Error extracting text.'
  
 if __name__ == '__main__':
     app.run()
