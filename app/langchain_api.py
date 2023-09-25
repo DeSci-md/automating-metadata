5a85a166311fd85a4979702299caad123ccac114
Harrison Loh <hankman.loh@gmail.com>
2023-09-25 10:32:54 -0400

    Add jsonformer draft function to langchain_api.py
    Add a draft function to fill out for taking the unstructured dictionary text from langchain_paper_search and returned a structured json-ld output according to a given template.

--- app/langchain_api.py
+++ app/langchain_api.py
@@ -251,6 +251,17 @@ async def langchain_paper_search(file_path):
     return llm_output
 
 
+def jsonformer_structure(unstructured_dict):
+    """
+    Take in unstructured text contained in the dictionary output of langchain_paper_search
+    and return structured json-ld using huggingface combined with jsonformer
+    """
+    structured_dict = 1
+    #TODO: make code to do this
+
+    return structured_dict
+
+
 def pdfprocess(file_path): 
     results = pdfMetadata(file_path)
     print(results)
