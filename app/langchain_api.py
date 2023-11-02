ddcb1276e5c01cf77dcca607fabacc65c9dc7104
Elianna DeSota <desotaelianna@gmail.com>
2023-11-02 14:38:26 -0400

    docker up and running

--- app/langchain_api.py
+++ app/langchain_api.py
@@ -10,6 +10,7 @@ import os
 from pathlib import Path  # directory setting
 import asyncio # For async asking of prompts
 import json
+from langchain.docstore.document import Document
 
 import httpx  # for elsevier and semantic scholar api calls
 from habanero import Crossref, cn  # Crossref database accessing
@@ -27,6 +28,8 @@ import fitz #pdf reading library
 import json
 from pyalex import Works #, Authors, Sources, Institutions, Concepts, Publishers, Funders
 import pyalex
+import PyPDF2
+import io
 #from demo import read_single 
 
 #TODO: IF doi -> then search open alex -> determine relevant metadata to return. -> Together once everything is up to date. 
@@ -40,7 +43,73 @@ pyalex.config.email = "ellie@desci.com"
 
 # Load in API keys from .env file
 load_dotenv(find_dotenv())
+import tiktoken
 
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    encoding = tiktoken.encoding_for_model(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+print(num_tokens_from_string("Hello world, let's test tiktoken.", "gpt-3.5-turbo"))
+
+def get_jsonld(node):
+    base = "https://beta.dpid.org/"
+    root = "?jsonld"
+
+    #return requested node JSON
+    response2 = requests.get(base+node+root).json()
+    return response2
+
+def get_pdf_text(node):
+    base = "https://beta.dpid.org/"
+    root = "?raw"
+
+    #return most recent node JSON
+    manifest = requests.get(base+node+root).json()
+    
+    #get the CID associated with the Payload + PDF object
+    try:
+        pdf_path = next(item['payload']['path'] for item in manifest['components'] if item['type'] == 'pdf')
+        print(pdf_path)
+        
+        pdf_url = next(item['payload']['url'] for item in manifest['components'] if item['type'] == 'pdf')
+
+    except: 
+        return "No PDF object found"
+    
+    url = base+pdf_path+"?raw"
+    print(url)
+    ipfs="https://ipfs.desci.com/ipfs/"+pdf_url
+    print(ipfs)
+  
+    response = requests.get(ipfs) 
+    
+    if response.status_code == 200:
+        # Open the PDF content with PyPDF2
+        pdf_file = PyPDF2.PdfReader(io.BytesIO(response.content))
+        
+        # Check if the PDF is extractable
+        if pdf_file.is_encrypted:
+            pdf_file.decrypt("")  # Assuming the PDF has no password
+            
+        
+        # Initialize text
+        pdf_text = ""
+        
+        # Limit processing to 7 pages
+        num_pages = min(len(pdf_file.pages), 7)
+        
+        for i in range(num_pages):
+            page = pdf_file.pages[i]
+            page_text = page.extract_text()
+            
+            if page_text is not None:
+                pdf_text += page_text
+            
+        return pdf_text
+    else:
+        print(f"Error fetching PDF from {url}. Status code: {response.status_code}")
+        return None
 
 def paper_data_json_single(doi):
     """
@@ -201,20 +270,41 @@ def paper_data_json_single(doi):
     openalex_results = Works()[doi]
     try:
         openalex_id = openalex_results['id']
-    except: openalex_id = "None, OpenAlex Lookup error"
+    except: 
+        openalex_id = "None, OpenAlex Lookup error"
 
-    if title == "None, Crossref Error":  # attempt replacing error title from cross with title from openalex
+    if "Error" in title:  # attempt replacing error title from cross with title from openalex
         try:
             title = openalex_results['title']
         except:
             pass
+    if "Error" in type:  # attempt replacing error keywords from cross with title from openalex
+        try:
+            type = openalex_results['type']
+        except:
+            pass
+    if "Error" in pub_name:  # attempt replacing error keywords from cross with title from openalex
+        try:
+            pub_name = openalex_results['primary_location']
+        except:
+            pass
 
-    if keywords == "None, Crossref Error":  # attempt replacing error title from cross with title from openalex
+    if "Error" in pub_date:  # attempt replacing error keywords from cross with title from openalex
+        try:
+            pub_date = openalex_results['publication_date']
+        except:
+            pass
+
+    if "Error" in keywords:  # attempt replacing error keywords from cross with title from openalex
         try:
             title = openalex_results['title']
         except:
             pass
-
+    
+    try:
+        openaccess = openalex_results['open_access']
+    except:
+        pass
     #%% Constructing output dictionary
     output_dict = {
         # Paper Metadata
@@ -234,14 +324,15 @@ def paper_data_json_single(doi):
         'tldr':tldr,
         'original_text':original_text,
         'openAccessPdf':openaccess_pdf,
-        'URL_link':url_link,
-        'openalex_id': openalex_id
+        'URL_link': url_link,
+        'openalex_id': openalex_id,
+        'openaccess': openaccess
     }
    
     return output_dict
 
-
-async def langchain_paper_search(file_path):
+async def langchain_paper_search(node):
+    #file_path
     """
     Analyzes a pdf document defined by file_path and asks questions regarding the text
     using LLM's.
@@ -268,19 +359,20 @@ async def langchain_paper_search(file_path):
 
     #%% Extracting info from paper
     # Define the PDF document, load it in
-    loader = PyPDFLoader(str(file_path))  # convert path to string to work with loader
-    document = loader.load_and_split()
+    text = get_pdf_text(node)
+    document = Document(page_content = text)
 
     # Define all the queries and corresponding schemas in a list
     queries_schemas_docs = [
-        ("What are the experimental methods and techniques used by the authors? This can include ways that data was collected as well as ways the samples were synthesized.", document),
-        ("What is the scientific question, challenge, or motivation that the authors are trying to address?", document),
-        ("Provide a summary of the results and discussions in the paper. What results were obtained and what conclusions were reached?", document),
+        #("What are the experimental methods and techniques used by the authors? This can include ways that data was collected as well as ways the samples were synthesized.", document),
+        #("What is the scientific question, challenge, or motivation that the authors are trying to address?", document),
+        #("Provide a summary of the results and discussions in the paper. What results were obtained and what conclusions were reached?", document),
         ("Provide a summary of each figure described in the paper. Your response should be a one sentence summary of the figure description, \
          beginning with 'Fig. #  - description...'. For example:'Fig. 1 - description..., Fig. 2 - description..., Fig. 3 - description...'. Separate each figure description by a single newline.", document),
-        ("What future work or unanswered questions are mentioned by the authors?", document),
+        #("What future work or unanswered questions are mentioned by the authors?", document),
         ("Tell me who all the authors of this paper are. Your response should be a comma separated list of the authors of the paper, \
-         looking like 'first author name, second author name", document)
+         looking like 'first author name, second author name", document),
+        ("Tell me the title of this paper", document)
     ]
 
     tasks = []
@@ -293,107 +385,101 @@ async def langchain_paper_search(file_path):
     summary = await asyncio.gather(*tasks)
 
     # Extracting individual elements from the summary
-    methods, motive, results, figures, future, authors = summary  #NOTE: output to variables in strings
+    #methods, motive, results, future
+    figures, authors, title = summary  #NOTE: output to variables in strings
 
     llm_output = {
-        "motive": motive,
-        "method": methods,
+        #"motive": motive,
+        #"method": methods,
         "figures": figures,
-        "results": results,
-        "future": future,
-        "authors": authors
+        #"results": results,
+        #"future": future,
+        "authors": authors,
+        "title": title
     }
 
+    #transform outputs
     llm_output['figures'] = llm_output['figures'].split("\n")# using newline character as a split point.
     llm_output['authors'] = llm_output['authors'].split(', ') 
+    llm_output['authors'] = get_orcid(llm_output["authors"])
 
-    llm_output['authors'] = get_orchid(llm_output["authors"])
     return llm_output
 
-def get_orchid(authors): 
-    orchid = []
+def get_orcid(authors): 
+    orcid = []
     author_info = {}   
-    print(type(authors))
-    
+
     for author in authors: 
-        #try: 
-        url = "https://api.openalex.org/autocomplete/authors?q=" + author
-        response = json.loads(requests.get(url).text)
-        #except: 
-        #    print("Your author might not be registered with ORCHID")
+        try: 
+            url = "https://api.openalex.org/autocomplete/authors?q=" + author
+            response = json.loads(requests.get(url).text)
+        except: 
+            print(f"OpenAlex ORCID lookup returned error: {e}\n")
         
         if response["meta"]["count"] == 1: 
-            orchid = response["results"][0]["external_id"]
-            author_info[author] = {"orchid": orchid, "affiliation":response["results"][0]["hint"]}
+            orcid = response["results"][0]["external_id"]
+            author_info[author] = {"orcid": orcid, "affiliation":response["results"][0]["hint"]}
         elif response["meta"]["count"] == 0: #FAKE - Create a test so we can check if the return is valid. 
-            print("There are no ORCHID suggestions for this author")
+            print("There are no OrcID suggestions for this author")
         else: 
-            orchid = response["results"][0]["external_id"]
-            author_info[author] = {"orchid": orchid, "affiliation": response["results"][0]["hint"]}
+            orcid = response["results"][0]["external_id"]
+            author_info[author] = {"orcid": orcid, "affiliation": response["results"][0]["hint"]}
             #create an async function which ranks the authors based on the similarity to the paper. 
 
     return author_info
 
-def create_metadata_json(data):
-    metadata_list = []
+def update_json_ld(json_ld, new_data):
+    # Process author information
+    for key, value in new_data.items(): 
+        if key == "authors": 
+            for author_name, author_info in new_data.get("authors", {}).items():
+                orchid = author_info.get("orcid")
+                institution = author_info.get("affiliation")
 
-    def process_data(data):
-        payload_body = {}
-        for name, content in data.items():
-            if isinstance(content, dict):
-                payload_body[name] = process_data(content)
-            else:
-                payload_body[name] = content
-        return payload_body
+                creator_entry = {
+                    "@type": "Person",
+                    "name": author_name
+                }
 
-    for name, content in data.items():
-        payload = {
-            'name': name,
-            'body': process_data(content) if isinstance(content, dict) else content
-        }
+                if orchid:
+                    creator_entry["@id"] = {"@id": orchid}
 
-        metadata = {
-            'author': 'MetadataBot',
-            'annotation': '',
-            'payload': payload
-        }
+                if institution:
+                    creator_entry["affiliation"] = institution
 
-        metadata_list.append(metadata)
+                json_ld["@graph"].append(creator_entry)
+                json_ld["@graph"][1]["creator"].append(creator_entry)
+        else:
+            json_ld["@graph"][1][key.lower()] = value
 
-    return metadata_list
+        return json_ld
 
 
 #%% Main, general case for testing
 if __name__ == "__main__":
-    #TODO: Update to be relevant. 
-
     print("Starting code run...")
-    cwd = Path(__file__)
-    pdf_folder = cwd.parents[1] #.joinpath('.test_pdf')  # path to the folder containing the pdf to test
 
-    # File name of pdf in the .test_pdf folder for testing with code
-    file_name = "Navahas2018.pdf"  # test 1
-    # file_name = "Zhao et al_2023_Homonuclear dual-atom catalysts embedded on N-doped graphene for highly.pdf"  # test 2, too long
-    # file_name = "Ren_Dong_2022_Direct electrohydrodynamic printing of aqueous silver nanowires ink on.pdf"  # test 3
-    # file_name = "Chang et al_2022_Few-layer graphene as an additive in negative electrodes for lead-acid batteries.pdf"  # test 4
-    # file_name = "Zhang et al_2019_Highly Stretchable Patternable Conductive Circuits and Wearable Strain Sensors.pdf"  # test 5
-    # file_name = "Jepsen_2019_Phase Retrieval in Terahertz Time-Domain Measurements.pdf"  # test 6
+    node = os.getenv('NODE_ENV')
+    DOI_env = os.getenv('DOI_ENV')
+    
+    if node is not None:
+        print(f"NODE_ENVIRONMENT is set to: {node}")
+    else:
+        print("NODE_ENVIRONMENT is not set.")
 
-    # pdf_file_path = pdf_folder.joinpath(file_name)
+    json_ld = get_jsonld(node)
 
-    # llm_output = asyncio.run(langchain_paper_search(pdf_file_path))  # output of unstructured text in dictionary
+    if DOI_env: 
+        lookup_results = paper_data_json_single(DOI_env)
+        updated_json_ld = update_json_ld(json_ld, lookup_results)
+    else: 
+        updated_json_ld = json_ld
+
+    llm_output = asyncio.run(langchain_paper_search(node))# output of unstructured text in dictionary
+    updated_json_ld = update_json_ld(json_ld, llm_output)
    
-    doi = "https://doi.org/10.1002/adma.202208113"
-    lookup_results = paper_data_json_single(doi)
-
-    print(lookup_results)
-     
-    # json_result = create_metadata_json(llm_output)
-    # for item in json_result:
-    #     print(json.dumps(item, indent=4))
-
-    # print(json_result)
-
-    print(lookup_results)
+    #doi = "https://doi.org/10.1002/adma.202208113"
+    
+    print(updated_json_ld)
 
     print("Script completed")
